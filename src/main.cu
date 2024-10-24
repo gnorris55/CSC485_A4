9084ece79f8b3e24036ab18e1876ef2c7c2889bd
gnorris55 <graemenorris99@gmail.com>
2024-10-24 14:12:04 -0700

    minor fix

--- src/main.cu
+++ src/main.cu
@@ -10,10 +10,10 @@
 
 int main()
 {
-    std::size_t const n = 1024 * 1024;
+    std::size_t const n = 32*32;
     std::size_t const switch_at = 3 * (n >> 2);
 
-    //auto data = csc485b::a1::generate_uniform< element_t >(n);
+    auto data = csc485b::a1::generate_uniform< element_t >(n);
     csc485b::a1::cpu::run_cpu_baseline(data, switch_at, n);
     csc485b::a1::gpu::run_gpu_soln(data, switch_at, n);
 
